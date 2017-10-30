eaea7fef082f83540e1c803e3864047b3ac952d7
Isaí <gussaza@yahoo.com>
2017-10-30 11:07:51 -0600

    Add files via upload

---
+++ Sub.java
@@ -0,0 +1,25 @@
+
+package sub;
+
+
+public class Sub {
+
+  
+    public static void main(String[] args) {
+           String Str = "arroz";
+
+      System.out.print("Return Value :" );
+      System.out.println(Str.subSequence(0, 1) );
+
+      System.out.print("Return Value :" );
+      System.out.println(Str.subSequence(0, 2) );
+      System.out.print("Return Value :" );
+      System.out.println(Str.subSequence(0, 3) );
+      
+      System.out.print("Return Value :" );
+      System.out.println(Str.subSequence(0, 4) );
+      System.out.print("Return Value :" );
+      System.out.println(Str.subSequence(0,5) );
+   }
+}
+
